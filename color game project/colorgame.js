47fa367a11dc8778ee35e55a78158b29f61bbfe5
aliarslanansari <aliarslan1620@gmail.com>
2020-04-26 15:49:01 +0530

    updated colors project

--- color game project/colorgame.js
+++ color game project/colorgame.js
@@ -1,78 +1,65 @@
 var countSquares = 6;
-var colors = generateRandomColor(countSquares);
-// [
-//     'rgb(255, 0, 0)',
-//     'rgb(0, 0, 255)',
-//     'rgb(0, 255, 0)',
-//     'rgb(255, 255, 0)',
-//     'rgb(255, 0, 255)',
-//     'rgb(0, 255, 255)',
-// ];
-
+var colors = [];
+var pickedColor;
 var squares = document.querySelectorAll('.square')
-var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDis = document.getElementById("message");
-colorDisplay.textContent = pickedColor;
 var h1 = document.querySelector('h1');
 var resetC = document.getElementById('reset');
-var easyB = document.getElementById('easyB');
-var hardB = document.getElementById('hardB');
+var modeButtons = document.querySelectorAll('.mode');
+init();
 
+function init(){
+    for(var i = 0; i < modeButtons.length; i++){
+        modeButtons[i].addEventListener('click',function(){
+            modeButtons[0].classList.remove('selected');
+            modeButtons[1].classList.remove('selected');
+            this.classList.add('selected');
+            this.textContent === "Easy"? countSquares = 3: countSquares = 6;
+            resetF();
+        });
+    }
+    for(var i = 0; i < squares.length; i++){
+        squares[i].addEventListener('click',function(){
+            var clickedColor = this.style.backgroundColor;
+            if(clickedColor === pickedColor){
+                messageDis.textContent = "Correct";
+                resetC.textContent = 'Play Again?';
+                changeColor(clickedColor);
+                h1.style.backgroundColor = clickedColor;
+            }else{
+                this.style.backgroundColor = "#232323";
+                messageDis.textContent = "Try Again";
+    
+            }
+        });
+    }
+    resetF();
+} 
 
-easyB.addEventListener("click",function(){
-    easyB.classList.add('selected');
-    hardB.classList.remove('selected');
-    countSquares = 3;
+function resetF(){
     colors = generateRandomColor(countSquares);
     pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
+    h1.style.backgroundColor = 'steelblue';
+    resetC.textContent = 'New COlors';
+    colorDisplay.textContent = pickedColor.toUpperCase();
+    messageDis.textContent = "";
     for(var i = 0; i < squares.length; i++){
         if(colors[i]){
             squares[i].style.backgroundColor = colors[i];
+            squares[i].style.display = 'block'; 
         }else{
             squares[i].style.display = 'none';  
         }
-    }
-});
-hardB.addEventListener("click",function(){
-    hardB.classList.add('selected');
-    easyB.classList.remove('selected');
-    countSquares = 6;
-    colors = generateRandomColor(countSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for(var i = 0; i < squares.length; i++){
-        if(colors[i]){
-            squares[i].style.backgroundColor = colors[i];
-            squares[i].style.display = 'block';  
-        }
-    }
-})
-
-for(var i = 0; i < squares.length; i++){
-    squares[i].style.backgroundColor = colors[i];
-    squares[i].addEventListener('click',function(){
-        var clickedColor = this.style.backgroundColor;
-        if(clickedColor === pickedColor){
-            messageDis.textContent = "Correct";
-            resetC.textContent = 'Play Again?';
-            changeColor(clickedColor);
-            h1.style.backgroundColor = clickedColor;
-        }else{
-            this.style.backgroundColor = "#232323";
-            messageDis.textContent = "Try Again";
-
-        }
-    });
-} 
-
-
+    } 
+}
 resetC.addEventListener("click", function(){
     colors = generateRandomColor(countSquares);
     pickedColor = pickColor();
-    h1.style.backgroundColor = '#232323';
-    colorDisplay.textContent = pickedColor;
+    h1.style.backgroundColor = 'steelblue';
+    resetC.textContent = 'New COlors';
+    colorDisplay.textContent = pickedColor.toUpperCase();
+    messageDis.textContent = "";
     for(var i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = colors[i];
     }

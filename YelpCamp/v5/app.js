6e6fa6223febf3d2df12c34eccb879ad8ea93f24
Ali Arsalan Ansari <aliarslanansari@hotmail.com>
2020-05-09 01:36:49 +0530

    refactored

--- YelpCamp/v5/app.js
+++ YelpCamp/v5/app.js
@@ -4,6 +4,9 @@ var express     = require('express'),
     app         = express(),
     Campground = require('./models/campground'),
     seedDB = require('./seeds'),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User = require("./models/user"),
     Comment = require('./models/comment');
 
 
@@ -13,12 +16,23 @@ const config = {
     useNewUrlParser:    true,
 };
 mongoose.connect("mongodb://localhost:27017/yelp_camp_v3",config);
-
 var bodyParser = require("body-parser");
 app.set('view engine', 'ejs');  
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname+"/public"));
 seedDB();
+
+//passport configuration
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized:false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 // Campground.create({
